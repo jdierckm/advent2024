7c31d1fc7c61500c87443dba3e8e210aa56c2e4a
Jeff Dierckman <dierckman.jeff@gmail.com>
2024-12-10 19:20:28 -0500

    day6 part2

--- day6.go
+++ day6.go
@@ -53,14 +53,69 @@ type Point struct {
 
 // return the total number of spaces walked before leaving area
 // ri,ci = starting location, di = starting direction
-func patrol1(area []string, ri, ci int, di direction) int {
+func patrol1(area []string, ri, ci int, di direction) (int, []Point) {
 	s := len(area)
 	d := di
-	visited := map[Point]bool{Point{ri, ci}: true}
+	visited := map[Point]bool{{ri, ci}: true}
 	for r, c := ri, ci; inArea(r, c, s); r, c, d = move(area, s, r, c, d) {
 		visited[Point{r, c}] = true
 	}
-	return len(visited)
+	slc := make([]Point, len(visited))
+	i := 0
+	for k, _ := range visited {
+		slc[i] = k
+		i++
+	}
+	return len(visited), slc
+}
+
+type PointDir struct {
+	x, y int
+	d    direction
+}
+
+func isLoopPath(area []string, ri, ci int, di direction) bool {
+	//we are in a loop if we come back to the same point
+	//and traveling in same direction
+	s := len(area)
+	d := di
+	visited := map[PointDir]bool{}
+	for r, c := ri, ci; inArea(r, c, s); r, c, d = move(area, s, r, c, d) {
+		p := PointDir{r, c, d}
+		_, ok := visited[p]
+		if ok {
+			return true
+		}
+		visited[PointDir{r, c, d}] = true
+	}
+	return false
+}
+
+func addObstacle(area []string, r, c int) {
+	u := []rune(area[r])
+	u[c] = '#'
+	area[r] = string(u)
+}
+
+func removeObstacle(area []string, r, c int) {
+	u := []rune(area[r])
+	u[c] = '.'
+	area[r] = string(u)
+}
+
+func patrol2(area []string, path []Point, ri, ci int, di direction) int {
+	sum := 0
+	for _, p := range path {
+		if (p.x == ri) && (p.y == ci) {
+			continue
+		}
+		addObstacle(area, p.x, p.y)
+		if isLoopPath(area, ri, ci, di) {
+			sum++
+		}
+		removeObstacle(area, p.x, p.y)
+	}
+	return sum
 }
 
 func day6() {
@@ -87,6 +142,8 @@ func day6() {
 		i++
 	}
 	println(r, ":", c)
-	println(patrol1(area, r, c, N))
+	cnt, path := patrol1(area, r, c, N)
+	println(cnt)
+	println(patrol2(area, path, r, c, N))
 
 }

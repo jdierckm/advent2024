0a9efd684e9073e486b76458914dd097df12b72a
Jeff Dierckman <dierckman.jeff@gmail.com>
2024-12-16 19:44:00 -0500

    day8

--- internal/day8/day8.go
+++ internal/day8/day8.go
@@ -1,6 +1,7 @@
 package day8
 
 import (
+	"aoc/internal/util"
 	"bufio"
 	"os"
 )
@@ -28,8 +29,104 @@ func showAntenna(p []string) {
 	}
 }
 
+type Point struct {
+	x, y int
+}
+
+func inRange(N, r, c int) bool {
+	if (r >= 0) && (r < N) && (c >= 0) && (c < N) {
+		return true
+	}
+	return false
+}
+
+func isAntennaRange(puzzle []string, N, r, c int) bool {
+	return inRange(N, r, c) && isAntenna(puzzle[r][c])
+}
+
+func antennaMap(p []string) (int, map[rune][]Point) {
+	m := make(map[rune][]Point)
+	for x, l := range p {
+		for y, v := range l {
+			if v != '.' {
+				_, ok := m[v]
+				if !ok {
+					m[v] = []Point{}
+				}
+				m[v] = append(m[v], Point{x, y})
+			}
+		}
+	}
+	return len(p), m
+}
+
+func antiNodes(size int, m map[rune][]Point) map[rune]*util.Set[Point] {
+	n := make(map[rune]*util.Set[Point])
+	for k, v := range m {
+		n[k] = util.NewSet[Point]()
+		for _, p := range v {
+			for _, p2 := range v {
+				if p != p2 {
+					deltaX := p.x - p2.x
+					deltaY := p.y - p2.y
+
+					pt := Point{p.x + deltaX, p.y + deltaY}
+					if inRange(size, pt.x, pt.y) {
+						n[k].Add(pt)
+					}
+
+					pt = Point{p2.x - deltaX, p2.y - deltaY}
+					if inRange(size, pt.x, pt.y) {
+						n[k].Add(pt)
+					}
+				}
+			}
+		}
+	}
+	return n
+}
+
+func antiNodes2(size int, m map[rune][]Point) map[rune]*util.Set[Point] {
+	n := make(map[rune]*util.Set[Point])
+	for k, v := range m {
+		n[k] = util.NewSet[Point]()
+		for _, p := range v {
+			for _, p2 := range v {
+				if p != p2 {
+
+					for d := -size; d < size+1; d++ {
+						deltaX := d * (p.x - p2.x)
+						deltaY := d * (p.y - p2.y)
+
+						pt := Point{p.x + deltaX, p.y + deltaY}
+						if inRange(size, pt.x, pt.y) {
+							n[k].Add(pt)
+						}
+
+						pt = Point{p2.x - deltaX, p2.y - deltaY}
+						if inRange(size, pt.x, pt.y) {
+							n[k].Add(pt)
+						}
+					}
+				}
+			}
+		}
+	}
+	return n
+}
+
+func uniq(m map[rune]*util.Set[Point]) int {
+	s := util.NewSet[Point]()
+	for _, nodes := range m {
+		for _, p := range nodes.Elements() {
+			s.Add(p)
+		}
+	}
+	return len(s.Elements())
+}
+
 func Run() {
-	file, _ := os.Open("../internal/day8/test")
+	file, _ := os.Open("../internal/day8/input")
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
@@ -43,8 +140,18 @@ func Run() {
 		puzzle = append(puzzle, string(line))
 	}
 
-	N := len(puzzle)
-	println(N)
-	showAntenna(puzzle)
+	//N := len(puzzle)
+	//println(N)
+	//showAntenna(puzzle)
+	N, m := antennaMap(puzzle)
+	aNodes := antiNodes(N, m)
+	println(uniq(aNodes))
+
+	aNodes2 := antiNodes2(N, m)
+	println(uniq(aNodes2))
+
+	//r := antiNodes(puzzle, N, 2, 5)
+	//println(r)
+	//println(allAntiNodes(puzzle))
 
 }
--- internal/util/util.go
+++ internal/util/util.go
@@ -12,3 +12,25 @@ const (
 	W
 	NW
 )
+
+type Set[T comparable] struct {
+	elements map[T]bool
+}
+
+func (s *Set[T]) Add(v T) {
+	s.elements[v] = true
+}
+
+func (s *Set[T]) Elements() []T {
+	ret := []T{}
+	for k, _ := range s.elements {
+		ret = append(ret, k)
+	}
+	return ret
+}
+
+func NewSet[T comparable]() *Set[T] {
+	s := Set[T]{}
+	s.elements = map[T]bool{}
+	return &s
+}

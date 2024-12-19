d61e8d2d3aa0050018dd7bfa6e498f215a32c2fb
Jeff Dierckman <dierckman.jeff@gmail.com>
2024-12-18 23:27:13 -0500

    day9 - part two not correct yet

--- internal/day9/day9.go
+++ internal/day9/day9.go
@@ -54,6 +54,75 @@ func compact(d []int) []int {
 	return d
 }
 
+type slot struct {
+	loc, size int
+}
+
+func openSlots(d []int) []slot {
+	ret := []slot{}
+	size := 0
+	loc := 0
+	for i, v := range d {
+		if v != -1 {
+			if size > 0 {
+				ret = append(ret, slot{loc: loc, size: size})
+				size = 0
+			}
+		} else {
+			if size == 0 {
+				loc = i
+			}
+			size++
+		}
+	}
+	return ret
+}
+
+type file struct {
+	num, len, loc int
+}
+
+func fileLocs(d []int) []file {
+	ret := []file{}
+	l := 0
+	last := -1
+	for i := len(d) - 1; i >= 0; i-- {
+		di := d[i]
+		if di != last {
+			if last != -1 {
+				ret = append(ret, file{num: last, len: l, loc: i + 1})
+			}
+			l = 0
+		}
+		last = d[i]
+		l++
+	}
+	return ret
+}
+
+func getSlot(slots []slot, length int) int {
+	for i, v := range slots {
+		if v.size >= length {
+			r := v.loc
+			slots[i] = slot{loc: v.loc + length, size: v.size - length}
+			return r
+		}
+	}
+	return -1
+}
+
+func compact2(d []int, slots []slot, fileLocs []file) {
+	for _, f := range fileLocs {
+		idx := getSlot(slots, f.len)
+		if idx > 0 {
+			for i := 0; i < f.len; i++ {
+				d[idx+i] = f.num
+				d[f.loc+i] = -1
+			}
+		}
+	}
+}
+
 func checksum(compact []int) int64 {
 	var s int64
 	s = 0
@@ -70,9 +139,21 @@ func Run() {
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
-	line, _, _ := reader.ReadLine()
-	diskMap := parseDisk(line)
-	disk := disk(diskMap)
-	compact := compact(disk)
+	line, _ := reader.ReadString('\n')
+	diskMap := parseDisk([]byte(line))
+	dsk := disk(diskMap)
+	compact := compact(dsk)
 	println(checksum(compact))
+
+	//part2
+	diskMap = parseDisk([]byte(line))
+	dsk = disk(diskMap)
+	//fmt.Printf("map: %v\n", dsk)
+	slots := openSlots(dsk)
+	//fmt.Printf("slots: %v\n", slots)
+	fileLocs := fileLocs(dsk)
+	//fmt.Printf("files: %v\n", fileLocs)
+	compact2(dsk, slots, fileLocs)
+	//fmt.Printf("map: %v\n", dsk)
+	println(checksum(dsk))
 }
